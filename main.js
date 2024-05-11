78c762698fe7e1bfd356e72954badac7a798607b
Derrick Henderson <2019dwh@gmail.com>
2024-05-10 21:22:06 -0500

    Import 'errors', 'hideError', 'renderError'

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
+import { ERRORS, hideError, renderError } from "./src/lib";
+
+console.log(ERRORS);
+
 const formEls = [
   ...Array.from(document.querySelectorAll("input")),
   document.querySelector("#message"),
 ];
-console.log(formEls);
 
 const subButton = [document.querySelector("button[type='submit']")];
 
 formEls.forEach((el) => {
   el.addEventListener("blur", (e) => {
-    console.log("blur event", e.target.id);
+    console.log("blur event", e.target.id, e.target.value);
   });
 });
