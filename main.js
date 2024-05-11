39376984e000ee12c317826ffed10014e2e060cc
Derrick Henderson <2019dwh@gmail.com>
2024-05-10 21:57:45 -0500

    Change error statements to include if else statements

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
 import { ERRORS, hideError, renderError } from "./src/lib";
 
-console.log(ERRORS);
-
 const formEls = [
   ...Array.from(document.querySelectorAll("input")),
   document.querySelector("#message"),
 ];
 
-const subButton = [document.querySelector("button[type='submit']")];
-
 formEls.forEach((el) => {
   el.addEventListener("blur", (e) => {
-    ERRORS.find((error) => error.id === e.target.id).validate(e.target.value);
+    const elError = ERRORS.find((error) => error.id === e.target.id);
+    if (!elError.validate(e.target.value)) renderError(e.target, elError.msg);
+    else hideError(e.target);
+  });
+});
+
+formEls.forEach((el) => {
+  el.addEventListener("focus", (e) => {
+    const elError = ERRORS.find((error) => error.id === e.target.id);
+    elError.isTouched = true;
+    hideError(e.target);
   });
 });
